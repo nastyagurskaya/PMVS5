9db3b8b43bdfd9c2e84b873cbd9c4302b535d9bf
NastyaGurskaya <just.gurskaya@gmail.com>
2016-10-23 14:45:35 -0100

    Change function calc_exit and calc_init, add dev and proc

--- calcul.c
+++ calcul.c
@@ -1,21 +1,50 @@
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lakshmanan");
-MODULE_DESCRIPTION("A Simple Hello World module");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Nastya Gurskaya");
+MODULE_DESCRIPTION("Calculator");
+MODULE_VERSION("1");
 
-static int __init hello_init(void)
+#define PROC_FIRST "first"
+#define PROC_SECOND "second"
+#define PROC_OPERAND "operand"
+#define DEV_RESULT "result"
+
+#define PROC_MAX_SIZE 20
+
+static struct proc_dir_entry *first_proc_file, *second_proc_file, *operand_proc_file;
+static char first_buffer[PROC_MAX_SIZE], second_buffer[PROC_MAX_SIZE], operand_buffer[PROC_MAX_SIZE];
+static unsigned long first_buffer_size = 0, second_buffer_size = 0, operand_buffer_size = 0;
+static const int first_proc_index = 1, second_proc_index = 2, operand_proc_index = 3;
+
+static ssize_t proc_write(struct file *filp, const char __user *buffer, size_t count, loff_t *data)
 {
-    printk(KERN_INFO "Hello world!\n");
+}
+static ssize_t dev_read(struct file * file, char * buf, size_t count, loff_t *ppos)
+{
+}
+
+static int calc_init(void)
+{
+    printk(KERN_INFO "Calc module started working\n");
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
-static void __exit hello_cleanup(void)
+static void calc_exit(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
+    	printk(KERN_INFO "Calc module stopped working\n");
+	remove_proc_entry(PROC_FIRST, NULL);
+	remove_proc_entry(PROC_SECOND, NULL);
+	remove_proc_entry(PROC_OPERAND, NULL);
+	misc_deregister(&result_dev);
 }
 
-module_init(hello_init);
-module_exit(hello_cleanup);
+module_init(calc_init);
+module_exit(calc_exit);
