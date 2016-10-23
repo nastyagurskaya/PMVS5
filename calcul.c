5d734c4b7111fc7ac5a7bcad47188c06b827bdaa
NastyaGurskaya <just.gurskaya@gmail.com>
2016-10-23 18:24:07 +0200

    Imliments proc and dev, update calc_init functions

--- calcul.c
+++ calcul.c
@@ -26,14 +26,98 @@ static const int first_proc_index = 1, second_proc_index = 2, operand_proc_index
 
 static ssize_t proc_write(struct file *filp, const char __user *buffer, size_t count, loff_t *data)
 {
+	char *proc_buffer;
+	unsigned long* buffer_size;
+	const unsigned char *name = filp->f_path.dentry->d_name.name;
+
+	printk(KERN_INFO "proc_write called\n");
+	*buffer_size = count;
+	if (*buffer_size > PROC_MAX_SIZE ) {
+		*buffer_size = PROC_MAX_SIZE;
+	}
+	if ( copy_from_user(proc_buffer, buffer, *buffer_size) ) {
+		return -EFAULT;
+	}
+	
+	return *buffer_size;
 }
 static ssize_t dev_read(struct file * file, char * buf, size_t count, loff_t *ppos)
 {
+	char result[PROC_MAX_SIZE];
+	int len = 0;
+	int first = str_to_int(first_buffer, first_buffer_size);
+	int second = str_to_int(second_buffer, second_buffer_size);
+	int ok = 1;
+	int res = 0;
+	if (operand_buffer_size == 0) {
+		ok = 0;
+	} else {
+		switch(operand_buffer[0]) {
+			case '-':
+				res = first - second;
+				break;
+			case '+':
+				res = first + second;
+				break;
+			case '*':
+				res = first * second;
+				break;
+			case '/':
+				if (second == 0) {
+					ok = 0;
+					break;
+				}
+				res = first / second;
+				break;
+			default:
+				ok = 0;
+		}
+	}
+	if (count < len)
+		return -EINVAL;
+	if (*ppos != 0)
+		return 0;
+	if (count < len)
+		return -EINVAL;
+	if (*ppos != 0)
+		return 0;
+	if (copy_to_user(buf, result, len))
+		return -EINVAL;
+	*ppos = len;
+	return len;
 }
 
 static int calc_init(void)
 {
-    printk(KERN_INFO "Calc module started working\n");
+	printk(KERN_INFO "Calc module started working\n");
+	first_proc_file = proc_create_data(PROC_FIRST, 766, NULL, &proc_file_ops, (void*) 		&first_proc_index);
+	if (first_proc_file == NULL) {
+		printk(KERN_ERR "can't create first proc");
+		remove_proc_entry(PROC_FIRST, NULL);
+		return -ENOMEM;
+	}
+	second_proc_file = proc_create_data(PROC_SECOND, 766, NULL, &proc_file_ops, (void*) 		&second_proc_index);
+	if (second_proc_file == NULL) {
+		printk(KERN_ERR "can't create second proc");
+		remove_proc_entry(PROC_FIRST, NULL);
+		remove_proc_entry(PROC_SECOND, NULL);
+		return -ENOMEM;
+	}
+	operand_proc_file = proc_create_data(PROC_OPERAND, 766, NULL, &proc_file_ops, (void*) 		&operand_proc_index);
+	if (operand_proc_file == NULL) {
+		printk(KERN_ERR "can't create operand proc");
+		remove_proc_entry(PROC_FIRST, NULL);
+		remove_proc_entry(PROC_SECOND, NULL);
+		remove_proc_entry(PROC_OPERAND, NULL);
+		return -ENOMEM;
+	}	
+	if (misc_register(&result_dev)) {
+		printk(KERN_ERR "Unable to register \"result\" misc device\n");
+		remove_proc_entry(PROC_FIRST, NULL);
+		remove_proc_entry(PROC_SECOND, NULL);
+		remove_proc_entry(PROC_OPERAND, NULL);
+		return -ENOMEM;
+	}
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
