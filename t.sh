2c612aab63281776fb6faed2ed33742af308a447
NastyaGurskaya <just.gurskaya@gmail.com>
2016-10-23 20:48:09 +0300

    Add more tests

--- t.sh
+++ t.sh
@@ -9,3 +9,14 @@ function test {
 
 test 7 + 14 21
 test 1 + 11 12
+test -4 + 10 6
+test 20 / 2 10
+test 10 / -2 -5
+test 6 / 3 2
+test 12 '*' 3 36
+test 12 '*' -2 -24
+test -6 '*' -6 36
+test 8 - 21 -13
+test 13 - -27 40
+test 20 - 50 -30
+test 3 - 3 0
