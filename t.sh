bc3d647d28aa40bddfb2b47cfb22776fbc489e90
NastyaGurskaya <just.gurskaya@gmail.com>
2016-10-24 00:37:21 +0300

    Change test file

--- t.sh
+++ t.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+sudo sh l.sh
+
 function test {
-	echo "$1" > /proc/first
-	echo "$2" > /proc/operand
-	echo "$3" > /proc/second
-	res=`sudo cat /dev/result`
-	echo "$1$2$3=${res}"
+	 echo $1 > /dev/first
+    echo $2 > /dev/operator
+    echo $3 > /dev/second
+	result=$(cat /proc/result)
+    if [ "$4" == "$result" ]; then
+        echo "True! $1 $2 $3=$result"
+    else
+        echo "False! $1$2$3=$result"
+    fi
 }
 
 test 7 + 14 21
@@ -13,10 +19,12 @@ test -4 + 10 6
 test 20 / 2 10
 test 10 / -2 -5
 test 6 / 3 2
-test 12 '*' 3 36
-test 12 '*' -2 -24
-test -6 '*' -6 36
+test 12 m 3 36
+test 12 m -2 -24
+test -6 m -6 36
 test 8 - 21 -13
 test 13 - -27 40
 test 20 - 50 -30
 test 3 - 3 0
+
+sudo sh unl.sh
